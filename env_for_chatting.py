691ca8902f92d8e0eade0ebb48b08bad28810be0
marcodestefano <marco.destefano1@stud.unifi.it>
2025-06-27 22:40:16 +0200

    Add caption with Gemma3 , Add ClipScore and make the avg of ten scores

---
+++ env_for_chatting.py
@@ -0,0 +1,149 @@
+#we are using requests, that it has the same functionality of curl.
+import requests
+import os
+import subprocess
+import time
+import base64
+from PIL import Image
+from io import BytesIO
+
+#Define the URL for the port
+URL_OLLAMA = "http://localhost:11434"
+#DEFINE string in input
+question = "Generate a single natural paragraph describing the image. Do not include headings, bullet points, or additional questions. Focus only on what is visually present."
+
+def image_to_base64(image_path):
+    with Image.open(image_path) as img:
+        img = img.convert("RGB")  # assicurati RGB
+        buffered = BytesIO()
+        img.save(buffered, format="JPEG")
+        return base64.b64encode(buffered.getvalue()).decode("utf-8")
+
+def choose_img(class_image, index):
+    path = f"./Small-ImageNet-Validation-Dataset-1000-Classes/ILSVRC2012_img_val_subset/{class_image}/"
+    files = sorted(os.listdir(path))
+    count=0
+    find=False
+    for f in files:
+        if count==index:
+            find=True
+            break
+        count=count+1
+
+    if find == False:
+        print("Error, no file is present with those class and index")
+        return None
+    else:
+        return path + f
+
+
+def download_dataset():
+    dataset_dir = "./Small-ImageNet-Validation-Dataset-1000-Classes"
+    if not os.path.exists(dataset_dir):
+        print("📦 Clonazione del dataset da GitHub...")
+        subprocess.run(["git", "clone", "https://github.com/ndb796/Small-ImageNet-Validation-Dataset-1000-Classes.git"])
+    else:
+        print("✅ Dataset già presente.")
+def chat_with_model( message_prompt, data_image, model_name):
+    setup_model(model_name)
+    if(data_image == None):
+        try:
+            response = requests.post("http://localhost:11434/api/generate",
+                                 json={ "model": model_name,"prompt": message_prompt,"stream": False,})
+        except requests.exceptions.ConnectionError:
+            pass
+
+        response_from_chat= response.json()
+        return response_from_chat["response"].strip()
+    else:
+        try:
+            response = requests.post("http://localhost:11434/api/generate",
+                                     json={"model": model_name, "prompt": message_prompt, "stream": False,"images": [data_image]})
+            response_from_chat = response.json()
+            print(f"[DEBUG] Risposta da '{model_name}':", response_from_chat)
+
+            # Verifica chiave 'response'
+            if "response" in response_from_chat:
+                return response_from_chat["response"]
+            else:
+                raise KeyError(f"'response' non presente. Risposta ricevuta: {response_from_chat}")
+        except requests.exceptions.ConnectionError:
+            pass
+
+# To modify the model name that ends always with :latest
+def delete_last_part(string):
+    return string.split(":")[0]
+
+# To initialize the ollama sw in a determinated port (11434)
+def start_ollama():
+    try:
+        requests.get("http://localhost:11434")
+        print("Ollama è già in esecuzione.")
+        return
+    except requests.exceptions.ConnectionError:
+        pass
+
+    print("Avvio Ollama in modalità 'serve'...")
+    subprocess.Popen(["ollama", "serve"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+    for _ in range(10):
+        try:
+            r = requests.get("http://localhost:11434")
+            if r.status_code in [200, 404]:
+                print("Ollama serve è attivo.")
+                return
+        except requests.exceptions.ConnectionError:
+            time.sleep(1)
+
+    raise RuntimeError("Ollama non si è avviato.")
+
+#With this funct we pull some models, choosing the name of LLM
+def setup_model(model_name):
+    print(f"Check if  '{ model_name }' is already present")
+    response= requests.get("http://localhost:11434/api/tags")
+    installed_models = []
+    for m in response.json().get("models", []):
+        installed_models.append(delete_last_part(m["name"]))
+    checked=False
+    for m in installed_models:
+        if (m == model_name):
+            checked=True
+            print("Modello già presente")
+            break
+    if checked == False:
+        print("Modello non presente")
+        pull_response= requests.post("http://localhost:11434/api/pull", json={"name": model_name}, stream=True)
+
+        if pull_response.status_code != 200:
+            raise RuntimeError(f"Pull fallito per il modello '{model_name}'. Codice Errore: '{pull_response.status_code}'")
+        else:
+            for line in pull_response.iter_lines():
+                if line:
+                    print(line.decode('utf-8'))
+
+            print(f" Modello '{model_name}' installato con successo.")
+
+#This function is used to setup a chat and use it.
+#It returns an array of caption in output from model.
+
+def make_all_conversation(models, img):
+    download_dataset()
+    start_ollama()
+    img_path = choose_img(img["class"], img["photo"])
+    response={model: "" for model in models}
+    if img_path== None:
+        print("Error")
+    else:
+        # Now we want to generate a expansion of an image.
+        #img = Image.open(img_path).show()
+        img64=image_to_base64(img_path)
+        for m in range(len(models)):
+            print(models[m])
+            response[models[m]]= chat_with_model(question, img64 ,models[m])
+            #response = chat_with_model(question, img64, "erwan2/DeepSeek-Janus-Pro-7B")
+            #print(f"Question2: '{question}'")
+            #print(f"Response2: '{response}'")
+    Image.open(img_path).show()
+    print(f"Question: '{question}'")
+    print(f"Response: '{response}'")
+    return response
--- main.py
+++ main.py
@@ -1,144 +1,61 @@
-#we are using requests, that it has the same functionality of curl.
-import requests
-import os
-import subprocess
-import time
-import base64
 from PIL import Image
 
-#Define the URL for the port
-URL_OLLAMA = "http://localhost:11434"
-#DEFINE string in input
-question = "Expand the image"
+import env_for_chatting as efc
+import random as rdm
+import torch
+from transformers import CLIPProcessor, CLIPModel
 
-def image_to_base64(image_path):
-    with open(image_path,"rb") as image_file:
-        return base64.b64encode(image_file.read()).decode("utf-8")
+NUM_CLASS=1000
+NUM_IMG_4_CLASS=5
+model_name = "openai/clip-vit-base-patch16"
+
+def compute_clip_score(image_path, text):
+    image = Image.open(image_path).convert("RGB")
+    inputs = clip_processor(text=[text], images=image, return_tensors="pt", padding=True, truncation=True,
+    max_length=77)
+
+    with torch.no_grad():
+        outputs = clip_model(**inputs)
+        image_emb = outputs.image_embeds[0]
+        text_emb = outputs.text_embeds[0]
+
+    image_emb = image_emb / image_emb.norm()
+    text_emb = text_emb / text_emb.norm()
+    score = torch.dot(image_emb, text_emb).item()
+    return score
+
+def avg_score(score_dict):
+        avg_scores = {}
+        for model, scores in score_dict.items():
+            avg_scores[model] = sum(scores) / len(scores) if scores else 0
+        return avg_scores
 
 
-def choose_img(class_image, index):
-    path = f"./Small-ImageNet-Validation-Dataset-1000-Classes/ILSVRC2012_img_val_subset/{class_image}/"
-    files = sorted(os.listdir(path))
-    count=0
-    find=False
-    for f in files:
-        if count==index:
-            find=True
-            break
-        count=count+1
 
-    if find == False:
-        print("Error, no file is present with those class and index")
-        return None
-    else:
-        return path + f
-
-
-def download_dataset():
-    dataset_dir = "./Small-ImageNet-Validation-Dataset-1000-Classes"
-    if not os.path.exists(dataset_dir):
-        print("📦 Clonazione del dataset da GitHub...")
-        subprocess.run(["git", "clone", "https://github.com/ndb796/Small-ImageNet-Validation-Dataset-1000-Classes.git"])
-    else:
-        print("✅ Dataset già presente.")
-def chat_with_model( message_prompt, data_image, model_name):
-    setup_model(model_name)
-    if(data_image == None):
-        try:
-            response = requests.post("http://localhost:11434/api/generate",
-                                 json={ "model": model_name,"prompt": message_prompt,"stream": False,})
-        except requests.exceptions.ConnectionError:
-            pass
-
-        response_from_chat= response.json()
-        return response_from_chat["response"]
-    else:
-        try:
-            response = requests.post("http://localhost:11434/api/generate",
-                                     json={"model": model_name, "prompt": message_prompt, "stream": False,"images": [data_image]})
-            #print(response.json())
-            response_from_chat=response.json()
-            return response_from_chat["response"]
-        except requests.exceptions.ConnectionError:
-            pass
-
-# To modify the model name that ends always with :latest
-def delete_last_part(string):
-    return string.split(":")[0]
-
-# To initialize the ollama sw in a determinated port (11434)
-def start_ollama():
-    try:
-        requests.get("http://localhost:11434")
-        print("Ollama è già in esecuzione.")
-        return
-    except requests.exceptions.ConnectionError:
-        pass
-
-    print("Avvio Ollama in modalità 'serve'...")
-    subprocess.Popen(["ollama", "serve"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-
-    for _ in range(10):
-        try:
-            r = requests.get("http://localhost:11434")
-            if r.status_code in [200, 404]:
-                print("Ollama serve è attivo.")
-                return
-        except requests.exceptions.ConnectionError:
-            time.sleep(1)
-
-    raise RuntimeError("Ollama non si è avviato.")
-
-#With this funct we pull some models, choosing the name of LLM
-def setup_model(model_name):
-    print(f"Check if  '{ model_name }' is already present")
-    response= requests.get("http://localhost:11434/api/tags")
-    installed_models = []
-    for m in response.json().get("models", []):
-        installed_models.append(delete_last_part(m["name"]))
-    checked=False
-    for m in installed_models:
-        if (m == model_name):
-            checked=True
-            print("Modello già presente")
-            break
-    if checked == False:
-        print("Modello non presente")
-        pull_response= requests.post("http://localhost:11434/api/pull", json={"name": model_name}, stream=True)
-
-        if pull_response.status_code != 200:
-            raise RuntimeError(f"Pull fallito per il modello '{model_name}'. Codice Errore: '{pull_response.status_code}'")
-        else:
-            for line in pull_response.iter_lines():
-                if line:
-                    print(line.decode('utf-8'))
-
-            print(f" Modello '{model_name}' installato con successo.")
-
-# To stop the process that is on in the port 11434
-def stop_ollama():
-    subprocess.Popen(["pkill", "ollama"])
-    response=requests.get("http://localhost:11434")
-    if response.status_code!=200:
-        print("Connection is close")
 
 if __name__ == '__main__':
-    download_dataset()
-    start_ollama()
-    img_path = choose_img(1,3)
-    if img_path== None:
-        print("Error")
-    else:
-        # Now we want to generate a expansion of an image.
-        img=Image.open(img_path).show()
-        img64=image_to_base64(img_path)
-        response= chat_with_model(question, img64 , "llava")
-        print(f"Question1: '{question}'")
-        print(f"Response1: '{response}'")
-        response = chat_with_model(question, img64, "moondream")
-        print(f"Question2: '{question}'")
-        print(f"Response2: '{response}'")
-    stop_ollama()
+    models=["llava","moondream","gemma3"]
+    img_class=rdm.randrange(NUM_CLASS)
+    img_num=rdm.randrange(NUM_IMG_4_CLASS)
+
+    clip_model = CLIPModel.from_pretrained(model_name)
+    clip_processor = CLIPProcessor.from_pretrained(model_name)
+    score_clip = {model: [0] * 10 for model in models}
+
+    img={"class":img_class, "photo":img_num}
+    for i in range(10):
+        captions=efc.make_all_conversation(models,img)
+
+        score_clip["llava"][i] = compute_clip_score(efc.choose_img(img["class"],img["photo"]),captions["llava"])
+        score_clip["moondream"][i] = compute_clip_score(efc.choose_img(img["class"], img["photo"]), captions["moondream"])
+        score_clip["gemma3"][i] = compute_clip_score(efc.choose_img(img["class"], img["photo"]), captions["gemma3"])
+
+    average_scores=avg_score(score_clip)
+    print(f"[llava] CLIP-S: {average_scores['llava']:.3f}")
+    print(f"[moondream] CLIP-S: {average_scores['moondream']:.3f}")
+    print(f"[gemma3] CLIP-S: {average_scores['gemma3']:.3f}")
+
+
 
 
 
