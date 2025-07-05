2b89917aa1e468e4172c6ef0bbbcd2e8c706fbb0
marcodestefano <marco.destefano1@stud.unifi.it>
2025-07-05 09:19:02 +0200

    monitoring of the time to respond

--- env_for_chatting.py
+++ env_for_chatting.py
@@ -1,4 +1,6 @@
-#we are using requests, that it has the same functionality of curl.
+# we are using requests, that it has the same functionality of curl.
+import json
+
 import requests
 import os
 import subprocess
@@ -7,10 +9,11 @@ import base64
 from PIL import Image
 from io import BytesIO
 
-#Define the URL for the port
+# Define the URL for the port
 URL_OLLAMA = "http://localhost:11434"
-#DEFINE string in input
-question = "Generate a single natural paragraph describing the image. Do not include headings, bullet points, or additional questions. Focus only on what is visually present."
+# DEFINE string in input
+question = "Describe the phisical appearence of this word in a flowing paragraph, avoiding bullet points: "
+
 
 def image_to_base64(image_path):
     with Image.open(image_path) as img:
@@ -19,22 +22,34 @@ def image_to_base64(image_path):
         img.save(buffered, format="JPEG")
         return base64.b64encode(buffered.getvalue()).decode("utf-8")
 
+
 def choose_img(class_image, index):
     path = f"./Small-ImageNet-Validation-Dataset-1000-Classes/ILSVRC2012_img_val_subset/{class_image}/"
     files = sorted(os.listdir(path))
-    count=0
-    find=False
+    count = 0
+    find = False
     for f in files:
-        if count==index:
-            find=True
+        if count == index:
+            find = True
             break
-        count=count+1
+        count = count + 1
 
     if find == False:
         print("Error, no file is present with those class and index")
         return None
     else:
         return path + f
+
+
+def choose_class(class_image, index):
+    with open('./Small-ImageNet-Validation-Dataset-1000-Classes/imagenet_class_index.json', 'r',
+              encoding='utf-8') as file:
+        map_class = json.load(file)
+
+    map_class = list(map_class.items())
+    path_img = choose_img(class_image, index)
+    obj = {"class name": map_class[class_image][1][1], "path": path_img}
+    return obj
 
 
 def download_dataset():
@@ -44,35 +59,33 @@ def download_dataset():
         subprocess.run(["git", "clone", "https://github.com/ndb796/Small-ImageNet-Validation-Dataset-1000-Classes.git"])
     else:
         print("✅ Dataset già presente.")
-def chat_with_model( message_prompt, data_image, model_name):
+
+
+def chat_with_model(message_prompt, class_image, model_name):
     setup_model(model_name)
-    if(data_image == None):
-        try:
-            response = requests.post("http://localhost:11434/api/generate",
-                                 json={ "model": model_name,"prompt": message_prompt,"stream": False,})
-        except requests.exceptions.ConnectionError:
-            pass
+    input_string = message_prompt + class_image
+    input_string = input_string.replace("_", " ")
 
-        response_from_chat= response.json()
-        return response_from_chat["response"].strip()
-    else:
-        try:
-            response = requests.post("http://localhost:11434/api/generate",
-                                     json={"model": model_name, "prompt": message_prompt, "stream": False,"images": [data_image]})
-            response_from_chat = response.json()
-            print(f"[DEBUG] Risposta da '{model_name}':", response_from_chat)
+    try:
+        start = time.time()
+        response = requests.post("http://localhost:11434/api/generate",
+                                 json={"model": model_name, "prompt": input_string, "stream": False})
+        response_from_chat = response.json()
+        end = time.time()
+        t = end - start
+        # Verifica chiave 'response'
+        if "response" in response_from_chat:
+            return response_from_chat["response"], t
+        else:
+            raise KeyError(f"'response' non presente. Risposta ricevuta: {response_from_chat}")
+    except requests.exceptions.ConnectionError:
+        pass
 
-            # Verifica chiave 'response'
-            if "response" in response_from_chat:
-                return response_from_chat["response"]
-            else:
-                raise KeyError(f"'response' non presente. Risposta ricevuta: {response_from_chat}")
-        except requests.exceptions.ConnectionError:
-            pass
 
 # To modify the model name that ends always with :latest
 def delete_last_part(string):
     return string.split(":")[0]
+
 
 # To initialize the ollama sw in a determinated port (11434)
 def start_ollama():
@@ -97,25 +110,27 @@ def start_ollama():
 
     raise RuntimeError("Ollama non si è avviato.")
 
-#With this funct we pull some models, choosing the name of LLM
+
+# With this funct we pull some models, choosing the name of LLM
 def setup_model(model_name):
-    print(f"Check if  '{ model_name }' is already present")
-    response= requests.get("http://localhost:11434/api/tags")
+    print(f"Check if  '{model_name}' is already present")
+    response = requests.get("http://localhost:11434/api/tags")
     installed_models = []
     for m in response.json().get("models", []):
         installed_models.append(delete_last_part(m["name"]))
-    checked=False
+    checked = False
     for m in installed_models:
         if (m == model_name):
-            checked=True
+            checked = True
             print("Modello già presente")
             break
     if checked == False:
         print("Modello non presente")
-        pull_response= requests.post("http://localhost:11434/api/pull", json={"name": model_name}, stream=True)
+        pull_response = requests.post("http://localhost:11434/api/pull", json={"name": model_name}, stream=True)
 
         if pull_response.status_code != 200:
-            raise RuntimeError(f"Pull fallito per il modello '{model_name}'. Codice Errore: '{pull_response.status_code}'")
+            raise RuntimeError(
+                f"Pull fallito per il modello '{model_name}'. Codice Errore: '{pull_response.status_code}'")
         else:
             for line in pull_response.iter_lines():
                 if line:
@@ -123,26 +138,34 @@ def setup_model(model_name):
 
             print(f" Modello '{model_name}' installato con successo.")
 
-#This function is used to setup a chat and use it.
-#It returns an array of caption in output from model.
+
+# This function is used to setup a chat and use it.
+# It returns an array of caption in output from model.
+
+def to_format_string(string):
+    char = ["\n", "\\", "/", "**"]
+    for c in char:
+        string = string.replace(c, " ")
+    return string
+
 
 def make_all_conversation(models, img):
     download_dataset()
     start_ollama()
-    img_path = choose_img(img["class"], img["photo"])
-    response={model: "" for model in models}
-    if img_path== None:
+    # img_path = choose_img(img["class"], img["photo"])
+    return_value = choose_class(img["class"], img["photo"])
+    response = {model: {"response": "", "response_time": 0.0} for model in models}
+    if return_value == None:
         print("Error")
     else:
         # Now we want to generate a expansion of an image.
-        #img = Image.open(img_path).show()
-        img64=image_to_base64(img_path)
-        for m in range(len(models)):
-            print(models[m])
-            response[models[m]]= chat_with_model(question, img64 ,models[m])
-            #response = chat_with_model(question, img64, "erwan2/DeepSeek-Janus-Pro-7B")
-            #print(f"Question2: '{question}'")
-            #print(f"Response2: '{response}'")
+
+        img64 = image_to_base64(return_value["path"])
+        for m in models:
+            resp, tm = chat_with_model(question, return_value["class name"], m)
+            response[m]["response_time"] = tm
+            response[m]["response"] = to_format_string(resp)
+
     print(f"Question: '{question}'")
     print(f"Response: '{response}'")
     return response
--- main.py
+++ main.py
@@ -5,14 +5,16 @@ import random as rdm
 import torch
 from transformers import CLIPProcessor, CLIPModel
 
-NUM_CLASS=1000
-NUM_IMG_4_CLASS=5
+NUM_CLASS = 1000
+NUM_IMG_4_CLASS = 5
+LEN = 10
 model_name = "openai/clip-vit-base-patch16"
+
 
 def compute_clip_score(image_path, text):
     image = Image.open(image_path).convert("RGB")
     inputs = clip_processor(text=[text], images=image, return_tensors="pt", padding=True, truncation=True,
-    max_length=77)
+                            max_length=77)
 
     with torch.no_grad():
         outputs = clip_model(**inputs)
@@ -24,40 +26,45 @@ def compute_clip_score(image_path, text):
     score = torch.dot(image_emb, text_emb).item()
     return score
 
+
 def avg_score(score_dict):
-        avg_scores = {}
-        for model, scores in score_dict.items():
-            avg_scores[model] = sum(scores) / len(scores) if scores else 0
-        return avg_scores
+    avg_scores = {}
+    for model, values in score_dict.items():
+        scores = values["clipscores"]
+        times = values["response_time"]
 
+        avg_clip = sum(scores) / len(scores) if scores else 0
+        avg_time = sum(times) / len(times) if times else 0
 
+        avg_scores[model] = {"avg_clipscore": avg_clip, "avg_time": avg_time}
+    return avg_scores
 
 
 if __name__ == '__main__':
-    models=["llava","moondream","gemma3"]
-    img_class=rdm.randrange(NUM_CLASS)
-    img_num=rdm.randrange(NUM_IMG_4_CLASS)
+    models = ["mistral", "phi3", "llama3.2"]
+    img_class = rdm.randrange(NUM_CLASS)
+    img_num = rdm.randrange(NUM_IMG_4_CLASS)
 
     clip_model = CLIPModel.from_pretrained(model_name)
     clip_processor = CLIPProcessor.from_pretrained(model_name)
-    score_clip = {model: [0] * 10 for model in models}
+    score_clip = {model: {"clipscores": [0] * LEN, "response_time": [0.0] * LEN} for model in models}
 
-    img={"class":img_class, "photo":img_num}
-    Image.open(efc.choose_img(img["class"],img["photo"])).show()
-    for i in range(10):
-        captions=efc.make_all_conversation(models,img)
+    img = {"class": img_class, "photo": img_num}
+    Image.open(efc.choose_img(img["class"], img["photo"])).show()
+    for i in range(LEN):
+        print(f"ITERATION N°: {i}")
+        captions = efc.make_all_conversation(models, img)
+        score_clip["mistral"]["clipscores"][i] = compute_clip_score(efc.choose_img(img["class"], img["photo"]), captions["mistral"]["response"])
+        score_clip["mistral"]["response_time"][i] = captions["mistral"]["response_time"]
+        score_clip["phi3"]["clipscores"][i] = compute_clip_score(efc.choose_img(img["class"], img["photo"]), captions["phi3"]["response"])
+        score_clip["phi3"]["response_time"][i] = captions["phi3"]["response_time"]
+        score_clip["llama3.2"]["clipscores"][i] = compute_clip_score(efc.choose_img(img["class"], img["photo"]), captions["llama3.2"]["response"])
+        score_clip["llama3.2"]["response_time"][i] = captions["llama3.2"]["response_time"]
 
-        score_clip["llava"][i] = compute_clip_score(efc.choose_img(img["class"],img["photo"]),captions["llava"])
-        score_clip["moondream"][i] = compute_clip_score(efc.choose_img(img["class"], img["photo"]), captions["moondream"])
-        score_clip["gemma3"][i] = compute_clip_score(efc.choose_img(img["class"], img["photo"]), captions["gemma3"])
-
-    average_scores=avg_score(score_clip)
-    print(f"[llava] CLIP-S: {average_scores['llava']:.3f}")
-    print(f"[moondream] CLIP-S: {average_scores['moondream']:.3f}")
-    print(f"[gemma3] CLIP-S: {average_scores['gemma3']:.3f}")
-
-
-
-
-
-
+    average_scores = avg_score(score_clip)
+    print(f"[mistral] CLIP-S: {average_scores['mistral']['avg_clipscore']:.3f}")
+    print(f"[mistral] Average Time to Respond: {average_scores['mistral']['avg_time']:.3f} seconds")
+    print(f"[phi3] CLIP-S: {average_scores['phi3']['avg_clipscore']:.3f}")
+    print(f"[phi3] Average Time to Respond: {average_scores['phi3']['avg_time']:.3f} seconds")
+    print(f"[llama3.2] CLIP-S: {average_scores['llama3.2']['avg_clipscore']:.3f}")
+    print(f"[llama3.2] Average Time to Respond: {average_scores['llama3.2']['avg_time']:.3f} seconds")
